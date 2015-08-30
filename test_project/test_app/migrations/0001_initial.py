ad6f2444377d8a3160e9df8438a5c107399fab0c
Nahuel Defosse <nahuel.defosse@gmail.com>
2015-08-30 13:58:56 -0300

    Inital migrations

---
+++ test_project/test_app/migrations/0001_initial.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+from django.conf import settings
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Board',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=50)),
+                ('participants', models.ManyToManyField(to=settings.AUTH_USER_MODEL)),
+            ],
+            options={
+                'verbose_name': 'Board',
+                'verbose_name_plural': 'Boards',
+            },
+        ),
+        migrations.CreateModel(
+            name='List',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('title', models.CharField(max_length=40)),
+                ('board', models.ForeignKey(to='test_app.Board')),
+            ],
+            options={
+                'verbose_name': 'List',
+                'verbose_name_plural': 'Lists',
+            },
+        ),
+        migrations.CreateModel(
+            name='Task',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('title', models.CharField(max_length=50)),
+                ('body', models.TextField()),
+            ],
+        ),
+    ]
